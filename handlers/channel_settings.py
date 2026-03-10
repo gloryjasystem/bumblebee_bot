90488a6e9d5ae491b49ab517b676349b53f28f51
Secvency <darkthemeofblume@gmail.com>
2026-03-10 13:53:34 +0200

    feat: open share dialog for Add Admin / Change Owner buttons

--- handlers/channel_settings.py
+++ handlers/channel_settings.py
@@ -3655,9 +3655,9 @@ async def _show_bs_team(callback: CallbackQuery, bot: Bot,
         "Выберите действие ⬇️",
         reply_markup=InlineKeyboardMarkup(inline_keyboard=[
             [InlineKeyboardButton(text="→ Добавить администратора",
-                                  url=admin_link)],
+                                  url=f"https://t.me/share/url?url={admin_link}&text=%D0%A7%D1%82%D0%BE%D0%B1%D1%8B+%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%B8%D1%82%D1%8C+%D1%82%D0%B5%D0%B1%D1%8F+%D0%B0%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BE%D0%BC+%D0%B1%D0%BE%D1%82%D0%B0%2C+%D0%BF%D0%B5%D1%80%D0%B5%D0%B9%D0%B4%D0%B8+%D0%BF%D0%BE+%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B5")],
             [InlineKeyboardButton(text="→ Сменить владельца",
-                                  url=owner_link)],
+                                  url=f"https://t.me/share/url?url={owner_link}&text=%D0%A7%D1%82%D0%BE%D0%B1%D1%8B+%D0%BF%D0%B5%D1%80%D0%B5%D0%B4%D0%B0%D1%82%D1%8C+%D1%82%D0%B5%D0%B1%D0%B5+%D0%BF%D1%80%D0%B0%D0%B2%D0%B0+%D0%B2%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%86%D0%B0+%D0%B1%D0%BE%D1%82%D0%B0%2C+%D0%BF%D0%B5%D1%80%D0%B5%D0%B9%D0%B4%D0%B8+%D0%BF%D0%BE+%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B5")],
             [InlineKeyboardButton(text="🔄 Обновить ссылки",
                                   callback_data=f"bs_team_refresh:{child_bot_id}")],
             *([
